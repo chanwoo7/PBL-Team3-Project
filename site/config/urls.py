7fc95179ebbdf3517f79cd72baedcad9b0bcb29e
hbgraph <hbgraph@naver.com>
2024-01-15 13:36:15 +0900

    hello world 완성

--- site/config/urls.py
+++ site/config/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from adApp import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('adApp/', views.index),
 ]
