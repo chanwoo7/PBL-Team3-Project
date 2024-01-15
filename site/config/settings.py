d5f63387215c02c7e64ac99574c6829d9959e510
chanwoo7 <chanwoo9987@gmail.com>
2024-01-15 18:50:45 +0900

    my_settings.py 생성

--- site/config/settings.py
+++ site/config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from . import my_settings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,16 +75,8 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'adserver',
-        'USER': 'pbl',
-        'PASSWORD': '1234',
-        'HOST': '10.223.117.164',
-        'PORT': 3306,
-    }
-}
+
+DATABASES = my_settings.DATABASES
 
 
 # Password validation
