8fd1f6e4bd781c23fe8f774aa235def0e0b546d6
jun <jyoung66130@naver.com>
2024-01-15 15:57:41 +0900

    models.py 모델 정의 및 migration

--- site/adApp/models.py
+++ site/adApp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Adv(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=50)
+    verified = models.BooleanField()
+
+
+class Ad(models.Model):
+    id = models.AutoField(primary_key=True)
+    advId = models.ForeignKey('Adv', on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    url = models.CharField(max_length=500)
+    type = models.CharField(max_length=10)
+    startTime = models.DateTimeField()
+    endTime = models.DateTimeField()
