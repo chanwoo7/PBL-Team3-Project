c82f5ad43754ea2ded6f09339349d302f6a16797
raxNone <dryou04@kookmin.ac.kr>
2024-01-15 17:36:05 +0900

    adApp/templates/adApp/main.html 업로드 및 urls.py 수정

--- site/adApp/urls.py
+++ site/adApp/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index,name='print'),
+    path('main/', views.main, name='main'),
 ]
--- site/adApp/views.py
+++ site/adApp/views.py
@@ -3,5 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+
 def index(request):
     return HttpResponse("Hello World!")
+
+def main(request):
+    return render(request,'adApp/main.html')
