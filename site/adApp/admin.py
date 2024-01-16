45da9fb2e141b4dccfbd31ac3019daf93b324435
jun <jyoung66130@naver.com>
2024-01-16 10:21:48 +0900

    django 관리자 설정 및 admin.py 모델 정보 추가

--- site/adApp/admin.py
+++ site/adApp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+
+# ------------------------------ #
+# DB 관리
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Adv)
+admin.site.register(Ad)
+
+# ------------------------------ #
