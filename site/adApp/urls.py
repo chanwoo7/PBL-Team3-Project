301b8bcd6f5b600356d8843595b8c54efca27d4d
raxNone <dryou04@kookmin.ac.kr>
2024-01-17 10:45:19 +0900

    css, js 연동

--- site/adApp/urls.py
+++ site/adApp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 
 from . import views
 
 urlpatterns = [
     path('', views.index,name='print'),
     path('main/', views.main, name='main'),
-    path('adManager/', views.adManager, name='manager')
+    path('adManager/', views.adManager, name='manager'),
 ]
