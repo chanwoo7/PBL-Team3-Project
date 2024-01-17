b8725d34c719e603569f3fa73aa00c3285e5ccaa
raxNone <dryou04@kookmin.ac.kr>
2024-01-17 21:53:09 +0900

    ADD, edit, del API 구현(ADD,edit은 수정 필요)

--- site/adApp/static/js/adTableModals.js
+++ site/adApp/static/js/adTableModals.js
@@ -1,18 +1,27 @@
+//import requestAPI from '{% static "./request.js" %}' ;
+//import requestAPI from './request.js';
+//import id from '{% static "./adTable.js" %}';
+
+
+
 const detailO = document.getElementsByClassName('detailO');
 const editO = document.getElementsByClassName('editO');
 const delO = document.getElementsByClassName('delO');
 
-const detailC = document.getElementsByClassName('detailC');
+const detailC = document.getElementsByClassName('detailC'); // 0:확인, 1:취소
 const editC = document.getElementsByClassName('editC');
 const delC = document.getElementsByClassName('delC');
 
 const detail = document.getElementById('detailContainer');
 const edit = document.getElementById('editContainer');
 const del = document.getElementById('delContainer');
-console.log(detailC)
-for(let i=0; i<detailO.length; i++){
+
+
+let adId=10; //값 받아와야함
+for(let i=0; i<detailO.length; i++){    
     detailO[i].addEventListener('click', () =>{
         detail.classList.remove('hidden');
+        //adId=id[i];
     });
     editO[i].addEventListener('click', () =>{
         edit.classList.remove('hidden');
@@ -20,8 +29,8 @@ for(let i=0; i<detailO.length; i++){
     delO[i].addEventListener('click', () =>{
         del.classList.remove('hidden');
     });
-
 }
+
 detailC[0].addEventListener('click', () =>{
     detail.classList.add('hidden');
 });
@@ -30,13 +39,51 @@ detailC[1].addEventListener('click', () =>{
 });
 editC[0].addEventListener('click', () =>{
     edit.classList.add('hidden');
+    let adType = document.getElementById('editAdType');
+    adType = adType.options[adType.selectedIndex].text;
+    let adUrl = document.getElementById('editAdUrl').value;
+    let adName = document.getElementById('editAdTitle').value;
+    let advId = document.getElementById('editAdvId').value;
+    let editArray = {
+        "name": adName,
+        "url": adUrl,
+        "type": adType,
+        "start_time": document.getElementById('editAdStartDate').value,
+        "end_time": document.getElementById('editAdEndTime').value,
+        "adv_id": advId,
+        "ad_price": document.getElementById('editAdPrice').value,
+    };
+    requestAPI('../api/edit_ad/'+adId+'/', 'PUT',editArray)
 });
 editC[1].addEventListener('click', () =>{
     edit.classList.add('hidden');
 });
 delC[0].addEventListener('click', () =>{
     del.classList.add('hidden');
+    requestAPI('../api/delete_ad/'+adId+'/','DELETE')
+    
 });
 delC[1].addEventListener('click', () =>{
     del.classList.add('hidden');
 });
+
+function requestAPI(url,met,jsonArray={}){
+    const options = {
+        method: met,
+        headers: {
+            "Content-Type": "application/json",
+        },
+    };
+    if (met !== 'DELETE') {
+        options.body = JSON.stringify(jsonArray);
+    }
+
+    fetch(url, options)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.error('Error :', error);
+        });
+}
--- site/adApp/static/js/headerModals.js
+++ site/adApp/static/js/headerModals.js
@@ -1,7 +1,7 @@
 const ADDo = document.getElementById('ADDo');
 const ADTo = document.getElementById('ADTo');
 
-const ADDc = document.getElementsByClassName('ADDc')//객체배열
+const ADDc = document.getElementsByClassName('ADDc')
 const ADTc = document.getElementsByClassName('ADTc')
 
 const ADD = document.getElementById('ADDcontainer');
@@ -9,14 +9,34 @@ const ADT = document.getElementById('ADTcontainer');
 
 ADDo.addEventListener('click', () => {
     ADD.classList.remove('hidden');
-    
 })
 ADTo.addEventListener('click', () => {
     ADT.classList.remove('hidden');
 })
 
-ADDc[0].addEventListener('click', () => {
+document.getElementById('ADDok').addEventListener('click', () => {
     ADD.classList.add('hidden');
+    alert();
+
+    let adType = document.getElementById('adType');
+    adType = adType.options[adType.selectedIndex].text;
+    let adUrl = document.getElementById('adUrl').value;
+    let adName = document.getElementById('adTitle').value;
+    let advId = document.getElementById('advId').value;
+    let editArray = {
+        "name": adName,
+        "url": adUrl,
+        "type": adType,
+        "start_time": document.querySelector('#adStartDate').value,
+        "end_time": document.querySelector('#adEndDate').value,
+        "adv_id": advId,
+        "ad_price": document.getElementById('adPrice').value,
+    };
+    console.log(editArray)
+    requestAPI('../api/add_ad/', 'POST',editArray)   
+
+    
+        
     
 })
 ADTc[0].addEventListener('click', () => {
@@ -31,3 +51,25 @@ ADTc[1].addEventListener('click', () => {
     ADT.classList.add('hidden');
     
 })
+
+
+function requestAPI(url,met,jsonArray={}){
+    const options = {
+        method: met,
+        headers: {
+            "Content-Type": "application/json",
+        },
+    };
+    if (met !== 'DELETE') {
+        options.body = JSON.stringify(jsonArray);
+    }
+
+    fetch(url, options)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            console.error('Error :', error);
+        });
+}
